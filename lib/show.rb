d9b7c8a43278c943205fd8a939944ab5c10cfbb8
Carson Jardine <carsonjardine@gmail.com>
2020-09-01 09:24:12 -0600

    Write method for highest paid actor and pass test

--- lib/show.rb
+++ lib/show.rb
@@ -13,4 +13,11 @@ class Show
     end
   end
 
+  def highest_paid_actor
+    highest_paid = @characters.max_by do |character|
+      character.salary
+    end
+    highest_paid.actor 
+  end
+
 end
