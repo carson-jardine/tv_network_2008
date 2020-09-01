7c8daa38d077bb79f8cbfda41764f3265c9b92cc
Carson Jardine <carsonjardine@gmail.com>
2020-09-01 09:37:22 -0600

    Write test to add shows

--- test/network_test.rb
+++ test/network_test.rb
@@ -7,6 +7,8 @@ require './lib/network'
 class NetworkTest < Minitest::Test
   def setup
     @nbc = Network.new("NBC")
+
+
   end
 
   def test_it_exists
@@ -20,35 +22,25 @@ class NetworkTest < Minitest::Test
   def test_shows_start_empty
     assert_equal [], @nbc.shows
   end
+
+  def test_it_can_add_shows
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    @nbc.add_show(knight_rider)
+    @nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], @nbc.shows 
+  end
 end
 
-# nbc.name
-# => "NBC"
 
-# nbc.shows
-# => []
 
-# michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-# => #<Character:0x00007fe5f88721f8...>
-
-# kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# => #<Character:0x00007fe5f8448f78...>
-
-# knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-# => #<Show:0x00007fe5f8398970...>
-
-# leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
-# => #<Character:0x00007fe5f832bb18...>
-
-# ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-# => #<Character:0x00007fe5f8172a60...>
-
-# parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
-# => #<Show:0x00007fe5f88b0a20...>
-
-# nbc.add_show(knight_rider)
-
-# nbc.add_show(parks_and_rec)
 
 # nbc.shows
 # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
